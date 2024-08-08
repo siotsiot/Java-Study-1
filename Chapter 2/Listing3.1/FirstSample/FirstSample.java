9fec9a98a9983b7396236949dfc4744bec1cd6d2
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-09 03:51:34 +0900

    Create FirstSample.java

---
+++ Chapter 2/Listing3.1/FirstSample/FirstSample.java
@@ -0,0 +1,12 @@
+/**
+ * 이것은 Core Java 3장에 나오는 첫 번째 샘플 프로그램입니다.
+ * @version 1.01 1997-03-22
+ * @author Gary Cornell
+ */
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("We will not use 'Hello, World!'");
+    }
+}
